7c8f68c573add7707ac11b0faa1fb68449b3589a
austinkret <austinkret@gmail.com>
2021-04-20 20:30:37 +0000

    counter variable refactored to use this

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,4 +1,13 @@
-$("tweet-text").ready(function() {
+$(document).ready(function() {
+  console.log('ready!');
   // --- our code goes here ---
-  
+  $("#tweet-text").keyup(function() {
+    const max = 140;
+    let length = $(this).val().length;
+    const counter = $(this).closest('.new-tweet').find(".counter");
+
+    counter.html(max - length);
+  });
 });
+
+$(".counter").change()
