c4e5b66a798ec5004114be40dc99b33f96c1bb7d
austinkret <austinkret@gmail.com>
2021-04-21 04:05:45 +0000

    comment added for clarity

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,6 +4,9 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+// TIME A GO FUNCTIONALITY
 $(document).read(function() {
   timeago.render(document.querySelectorAll('.need_to_be_rendered'));
 });
+
