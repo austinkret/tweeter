07a2d3897382f0a73029226fd8563a97de5c889e
austinkret <austinkret@gmail.com>
2021-04-22 22:16:05 +0000

    duplicate tweets fixed

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,72 +3,69 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
-// RENDER TWEETS FUNCTION
-const renderTweets = function(tweets) {
-  for (let values of tweets) {
-    $('#tweets-container').prepend(createTweetElement(values));
-  }
-};
-
-// PREVENT UNWANTED CROSS-SITE SCRIPTING
-const escape = function(str) {
-  let div = document.createElement("div");
-  div.appendChild(document.createTextNode(str));
-  return div.innerHTML;
-};
-
-// CREATE TWEET FUNCTION
-const createTweetElement = function(tweetData) {
-  const body = document.querySelector('body');
-
-  if (body.classList.contains('body-dark')) {
-    const $tweet =
-    `<article class="thread-container-dark">
-    <header class="tweet-thread-dark">
-      <div class="user-dark">
-        <img class="avatar-thread" src="${tweetData.user.avatars}">
-        <span class="name">${tweetData.user.name}</span>
-      </div>
-      <span class="username">${tweetData.user.handle}</span>
-    </header>
-    <span class="tweet-post-dark">${escape(tweetData.content.text)}</span>
-    <footer>
-      <span class="time-stamp-dark" class="need_to_be_rendered" datetime="1473245023718">${timeago.format(tweetData.created_at)}</span>
-      <div class=" icons">
-        <i class="fas fa-flag"></i>
-        <i class="fas fa-heart"></i>
-        <i class="fas fa-retweet"></i>
-      </div>
-    </footer>
-  </article>`;
-    return $tweet;
-  } else if (!body.classList.contains('body-dark')) {
-    const $tweet =
-    `<article class="thread-container">
-    <header class="tweet-thread">
-      <div class="user">
-        <img class="avatar-thread" src="${tweetData.user.avatars}">
-        <span class="name">${tweetData.user.name}</span>
-      </div>
-      <span class="username">${tweetData.user.handle}</span>
-    </header>
-    <span class="tweet-post">${escape(tweetData.content.text)}</span>
-    <footer>
-      <span class="time-stamp" class="need_to_be_rendered" datetime="1473245023718">${timeago.format(tweetData.created_at)}</span>
-      <div class=" icons">
-        <i class="fas fa-flag"></i>
-        <i class="fas fa-heart"></i>
-        <i class="fas fa-retweet"></i>
-      </div>
-    </footer>
-  </article>`;
-    return $tweet;
-  }
-};
 // DOCUMENT READY CALL
 $(document).ready(function() {
 
+  // PREVENT UNWANTED CROSS-SITE SCRIPTING
+  const escape = function(str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+
+  // CREATE TWEET FUNCTION
+  const createTweetElement = function(tweetData) {
+    const $tweet =
+  `<article class="thread-container">
+  <header class="tweet-thread">
+    <div class="user">
+      <img class="avatar-thread" src="${tweetData.user.avatars}">
+      <span class="name">${tweetData.user.name}</span>
+    </div>
+    <span class="username">${tweetData.user.handle}</span>
+  </header>
+  <span class="tweet-post">${escape(tweetData.content.text)}</span>
+  <footer>
+    <span class="time-stamp" class="need_to_be_rendered" datetime="1473245023718">${timeago.format(tweetData.created_at)}</span>
+    <div class=" icons">
+      <i class="fas fa-flag"></i>
+      <i class="fas fa-heart"></i>
+      <i class="fas fa-retweet"></i>
+    </div>
+  </footer>
+</article>`;
+    return $tweet;
+  };
+
+  // RENDER TWEETS FUNCTION
+  const renderTweets = function(tweets) {
+    for (let values of tweets) {
+      $('#tweets-container').prepend(createTweetElement(values));
+    }
+  };
+
+  // LOAD TWEETS FUNCTION
+  const loadTweets = function() {
+    $.getJSON('/tweets', { method: 'GET'})
+      .done(function(data) {
+        renderTweets(data);
+      })
+      .fail(function(error) {
+        console.log("We had trouble making your request due to an error: ", error);
+      });
+  };
+
+  loadTweets();
+
+  //LOAD NEW TWEETS
+  const loadNewTweets = function() {
+    $.getJSON('/tweets', { method: 'GET'})
+      .then((data) => {
+        $('#tweets-container').prepend(createTweetElement(data[data.length - 1]));
+      }).fail(function(error) {
+        console.log("We had trouble making your request due to an error: ", error);
+      });
+  };
   // ON SUBMIT OF THE FORM FUNCTION
   $('form').on('submit', function(event) {
     event.preventDefault();
@@ -93,13 +90,11 @@ $(document).ready(function() {
     }).then(function() {
       // GETJSON AS THE INPUT VALUE WILL BE A JSON ELEMENT
       $.getJSON('/tweets', { method: 'GET'})
-        .done(function(data) {
+        .done(function() {
           // WHEN DONE, LOAD TWEETS
-          loadTweets(data);
+          loadNewTweets();
           // RESET THE FORM TO EMPTY SO USER CAN TWEET AGAIN WITHOUT HAVING TO MANUALLY CLEAR THE FORM
           $('form')[0].reset();
-          // RESET CHARACTER COUNTER ON SUCCESSFUL SUBMIT
-          $('.counter').text('140');
           // APPEND A SUCCESS MESSAGE SO USER KNOWS THEIR TWEET WAS SUCCESSFUL
           return $('.warning').append('<span class="success"><i class="fas fa-check-circle"></i>Success! Look below for your tweet!</span>').hide().fadeIn();
         });
@@ -109,16 +104,6 @@ $(document).ready(function() {
     });
   });
   
-  // LOAD TWEETS FUNCTION
-  const loadTweets = function() {
-    $.getJSON('/tweets', { method: 'GET'})
-      .done(function(data) {
-        renderTweets(data);
-      })
-      .fail(function(error) {
-        console.log("We had trouble making your request due to an error: ", error);
-      });
-  };
-  loadTweets();
+
 
 });
