87e201279dc5b408ca8dbb36afc905ba6403874c
austinkret <austinkret@gmail.com>
2021-04-22 19:44:41 +0000

    cleanup of test code

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
-  console.log('ready!');
-  // --- our code goes here ---
-  $("#tweet-text").keyup(function() {
+
+  $(".tweet-text").keyup(function() {
     const max = 140;
     let length = $(this).val().length;
     const counter = $(this).siblings("div").children("output");
