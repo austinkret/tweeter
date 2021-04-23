106b9ba67742ec89392a00e02b32a56f4f6bd736
austinkret <austinkret@gmail.com>
2021-04-23 03:02:21 +0000

    clarified variable names, and added comments

--- public/scripts/scroll.js
+++ public/scripts/scroll.js
@@ -1,26 +1,31 @@
+// INTERSECTION OBERVER FUNCTION
 $(document).ready(function() {
 
   const nav = document.querySelector("nav");
-  const sectionOne = document.querySelector(".page-header");
+  const pageHeader = document.querySelector(".page-header");
 
-  const sectionOneOptions = {
+  // MARGIN WHICH OBSERVED IS NOW 60PX OFF THE ACTUAL
+  const pageHeaderOptions = {
     rootMargin: "-60px 0px 0px 0px"
   };
 
-  const sectionOneObserver = new IntersectionObserver(function(entries, sectionOneObserver) {
+  // CREATE NEW INTERSECTION OBSERVER FUNCTION
+  const pageHeaderObserver = new IntersectionObserver(function(entries) {
+    // LOOP THROUGH THE ENTRIES
     entries.forEach(entry => {
-      console.log(entry.target);
+      // IF THE PAGE HEADER IS NOT INTERSECTING ANYMORE, THE ADD THE NAV CLASS AND ADD THE AVATAR TO THE NAV
       if (!entry.isIntersecting) {
         nav.classList.add("nav-scrolled");
         $('.mini-avatar').append('<img class="nav-avatar" src="/images/profile-hex.png">').hide().fadeIn();
       } else {
+        // IF THE PAGE HEADER IS INTERSECTING AGAIN, REMOVE THE CLASS AND EMPTY THE AVATAR
         nav.classList.remove("nav-scrolled");
         $('.mini-avatar').empty().hide().fadeIn();
       }
     });
   },
-  sectionOneOptions);
+  pageHeaderOptions);
 
-  sectionOneObserver.observe(sectionOne);
+  pageHeaderObserver.observe(pageHeader);
 
 });
