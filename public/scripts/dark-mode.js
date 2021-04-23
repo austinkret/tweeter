3ccd839405f9b02c65e47f07fa344327a8e491d2
austinkret <austinkret@gmail.com>
2021-04-23 02:48:28 +0000

    dark mode toggle function comments added

--- public/scripts/dark-mode.js
+++ public/scripts/dark-mode.js
@@ -1,10 +1,13 @@
 
-$(document).ready(function() {
+// FUNCTIONALITY TO TOGGLE CLASSES BETWEEN DARK MODE AND STANDARD MODE
 
+$(document).ready(function() {
   const body = document.querySelector('body');
 
+  //CONDITION IF THE BODY TAG CONTAINS THE CLASS BODY-DARK, TOGGLE THE TARGETED CLASS WITH THE NORMAL CLASS
   if (body.classList.contains('body-dark')) {
-    $(".darkmode").click(function() { // darkmode for headline body, adds css styles on click
+    // WHEN DARK MODE BUTTON IS CLICKED
+    $(".darkmode").click(function() {
       $(".body-dark").toggleClass("body");
       $(".counter-dark").toggleClass("counter");
       $(".tweet-thread-dark").toggleClass("tweet-thread");
@@ -14,9 +17,14 @@ $(document).ready(function() {
       $(".time-stamp-dark").toggleClass("time-stamp");
       $(".tweet-text-dark").toggleClass("tweet-text");
       $(".thread-container-dark").toggleClass("thread-container");
+
+
     });
-  } else if (!body.classList.contains('body-dark')) {
-    $(".darkmode").click(function() { // darkmode for headline body, adds css styles on click
+
+    //IF THE BODY DOES NOT HAVE THE BODY DARK CLASS THEN TARGET THE CLASS AND ADD THE DARK CLASS
+  } else {
+    // WHEN DARK MODE BUTTON IS CLICKED
+    $(".darkmode").click(function() {
       $("body").toggleClass("body-dark");
       $(".counter").toggleClass("counter-dark");
       $(".tweet-thread").toggleClass("tweet-thread-dark");
